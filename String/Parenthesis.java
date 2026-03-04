d5e5e08e791cf437dbc6055ee0fdeed987f5654e
bh462007 <bh462007@users.noreply.github.com>
2026-03-04 23:16:28 +0530

    Valid parenthesis

---
+++ String/Parenthesis.java
@@ -0,0 +1,34 @@
+
+import java.util.Stack;
+
+class Parenthesis{
+    static boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.pop();
+                if((ch=='}' && top!='{' )|| (ch==']' && top!='[') || (ch==')' && top!='(')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+    public static void main(String[] args) {
+        String s="{[()]}";
+        if(isValid(s)){
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Invalid");
+        }
+    }
+}
