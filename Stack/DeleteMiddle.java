5cde40ffd964508d310b9c7f5c5963918a979e80
bh462007 <bh462007@users.noreply.github.com>
2026-04-26 21:27:06 +0530

    delete middle element without using recursion

--- Stack/DeleteMiddle.java
+++ Stack/DeleteMiddle.java
@@ -2,33 +2,78 @@
 import java.util.Stack;
 
 class DeleteMiddle{
-    static void deleteMiddle(Stack<Integer> stack, int k){
-        //if middle reached then pop
-        if(k==0){
-            stack.pop();
-            return;
+    //recursive way
+    // static void deleteMiddle(Stack<Integer> stack, int k){
+    //     //if middle reached then pop
+    //     if(k==0){
+    //         stack.pop();
+    //         return;
+    //     }
+    //     //pop the top element
+    //     int temp=stack.pop();
+
+    //     //call recursively
+    //     deleteMiddle(stack, k-1);
+
+    //      //push back elements
+    //     stack.push(temp);
+    // }
+
+    // public static void main(String[] args) {
+    //     Stack<Integer> stack=new Stack();
+    //     stack.push(1);
+    //     stack.push(2);
+    //     stack.push(3);
+    //     stack.push(4);
+    //     stack.push(5);
+
+    //     int k=stack.size()/2;
+
+    //     deleteMiddle(stack, k);
+
+    //     System.out.println(stack);
+
+    // }
+
+
+
+
+
+
+
+    //iterative way
+
+    static void deleteMiddle(Stack<Integer> stack){
+        int n=stack.size();
+        if(n==0) return;
+
+        int k=n/2;
+
+        Stack<Integer> temp=new Stack<>();
+
+        //moving element into temp stack until k
+        for (int i = 0; i < k; i++) {
+            temp.push(stack.pop());
         }
-        //pop the top element
-        int temp=stack.pop();
 
-        deleteMiddle(stack, k-1);
+        //pop middle
+        stack.pop();
 
-        stack.push(temp);
+        //push back remaining element
+        while(!temp.isEmpty()){
+            stack.push(temp.pop());
+        }
     }
 
     public static void main(String[] args) {
-        Stack<Integer> stack=new Stack();
+        Stack<Integer> stack=new Stack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(4);
-        stack.push(5);
 
-        int k=stack.size()/2;
-
-        deleteMiddle(stack, k);
+        deleteMiddle(stack);
 
         System.out.println(stack);
-
     }
 }
